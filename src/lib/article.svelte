cc36b66e3bf3c48f8eacdd340cd3be64a500c4d4
forrestbalman <ifork1@gmail.com>
2024-06-08 16:02:19 -0700

    images not working

--- src/lib/article.svelte
+++ src/lib/article.svelte
@@ -1,57 +1,170 @@
 <section class="text-light">
-    <div class="container">
-        <p class="fs-2 fw-medium text-center">What is negative harmony?</p>
-        <p class="fs-4 mb-3">Recently popularized by <a href="https://www.youtube.com/watch?v=aewI1F8bA8M" target="_blank">Jacob Collier</a>, negative harmony is a theory that suggests that translating a chord across a tonic/dominant axis will produce another chord with the same potential function due to its intervallic structure in relation to the tonic.</p>
-        <figure class="figure d-flex flex-column align-items-center mb-3">
-            <img class="img-fluid rounded-2" src="/media/images/apps/negative-harmony/axis-c.png" alt="The tonic dominant axis C" />
-            <figcaption class="figure-caption text-light">The tonic dominant axis for C</figcaption>
-        </figure>
-        <p class="fs-4">Although it's easier to visualize as a midway point between the tonic and dominant, the axis that we're translating across is at E-quarter-flat. If we take each chord tone and replace it with it counterpart across this axis, we'll generate our negative harmony. For example, A minor's negative counterpart is E-flat major. How would you figure this out without using the calculator 🤔? By translating each of the chord's tones across the axis:</p>
-        <ol class="mb-3">
-            <li class="fs-4 fw-light">A would be converted to B-flat</li>
-            <li class="fs-4 fw-light">C would be converted to G</li>
-            <li class="fs-4 fw-light">E would be converted to E-flat</li>
-        </ol>
-        <p class="fs-4 mb-3">If we follow the same procedure for all diatonic chords, we can make a chart that should be true for all keys 😲:</p>
-        <ul class="mb-3">
-            <li class="fs-4 fw-light"><span class="serif">I</span> turns into <span class="serif">i</span></li>
-            <li class="fs-4 fw-light"><span class="serif">ii</span> turns into ♭<span class="serif">VII</span></li>
-            <li class="fs-4 fw-light"><span class="serif">iii</span> turns into ♭<span class="serif">VI</span></li>
-            <li class="fs-4 fw-light"><span class="serif">IV</span> turns into <span class="serif">v</span></li>
-            <li class="fs-4 fw-light"><span class="serif">V</span> turns into <span class="serif">iv</span></li>
-            <li class="fs-4 fw-light"><span class="serif">vi</span> turns into ♭<span class="serif">III</span></li>
-            <li class="fs-4 fw-light"><span class="serif">vii°</span> turns into <span class="serif">ii°</span></li>
-        </ul>
-        <p class="fs-4 mb-3">Coincidentally, the chart above also covers the minor scale. Notice how the translations from right to left procude the chords of the natural minor scale. In the case of melodic minor, the chart accounts for <span class="serif">ii</span> and <span class="serif">vii°</span>, but not <span class="serif">vi°</span> and <span class="serif">III</span>+, because they require a chromatic alteration that isn't included in the parallel major.</p>
-        <ul class="mb-3">
-            <li class="fs-4"><span class="serif">vi°</span> turns into <span class="serif">iii°</span></li>
-            <li class="fs-4">♭<span class="serif">III</span>+ turns into ♭<span class="serif">VI</span>+</li>
-        </ul>
-        <p class="fs-4 mb-3">You can also use these shortcuts if you want to calculate negative harmonies on the fly:</p>
-        <ol class="mb-3">
-            <li class="fs-4 fw-light">To get the scale degree of the negative harmony, subtract your starting scale degree from 9. I.e 2 turns into 7 because 9 minus 2 is equal to 7.</li>
-            <li class="fs-4 fw-light">Major chords become minor, and vice versa.</li>
-            <li class="fs-4 fw-light">Diminished and augmented chords keep their quality.</li>
-        </ol>
-        <p class="fs-4 mb-3">With all that being said, you should be able to trust the chart, or the quick and dirty method, above for every key; however let's test it in the key of F major and see if the translations are correct.</p>
-        <figure class="figure d-flex flex-column align-items-center mb-3">
-            <img class="img-fluid rounded-2" src="/media/images/apps/negative-harmony/axis-f.png" alt="The tonic dominant axis for F" />
-            <figcaption class="figure-caption text-light">The tonic dominant axis for F</figcaption>
-        </figure>
-        <p class="fs-4 mb-3">Because we found the negative harmony for A minor in relation to C major in the first example, let's try D minor here. As we hypothesized above, <span class="serif">vi</span>'s negative counterpart should be ♭<span class="serif">III</span>.</p>
-        <ol class="mb-3">
-            <li class="fs-4 fw-light">D would be converted to E-flat</li>
-            <li class="fs-4 fw-light">F would be converted to C</li>
-            <li class="fs-4 fw-light">A would be converted to A-flat</li>
-        </ol>
-        <p class="fs-4 mb-3"><a href="https://en.wikipedia.org/wiki/Ernst_Levy" target="_blank">Ernst Levy</a> 👨‍🏫 is the theorist credited with discovering this and detailing his approach in his textbook <a class="fst-italic" href="https://en.wikipedia.org/wiki/Ernst_Levy" target="_blank">Theory of Harmony</a>. However, after reading the book myself, although Levy goes into detail about concepts such as <span class="fst-italic">polarity</span> and <span class="fst-italic">telluric gravity</span>, I couldn't find a reference to this specific approach, which involves taking the circle of fifths and rotating it to create an imaginary axis between E-flat and E-natural.</p>
-        <p class="fs-4 mb-3">Instead, on page 25 for instance, we see that C remains the axis, and that G translates to F, E to A-flat, etc. . Levy does comment on the idea of the label <span class="fst-italic">subdominant</span> being a fallacy due to both dominant and subdominant behaving similarly. But other than this, I can't seem to find a passage that suggests this exact method, despite it being the consensus method across the web 🌐.</p>
-        <p class="fs-4">In fact, I never saw the term <span class="fst-italic">negative harmony</span> used once; however, I think it is a fantastic way to illustrate the harmonic relationships being theorized here. 🎵</p>
-    </div>
+	<div class="container">
+		<p class="fs-2 fw-medium text-center">What is negative harmony?</p>
+		<p class="fs-4 mb-3">
+			Recently popularized by <a
+				href="https://www.youtube.com/watch?v=aewI1F8bA8M"
+				target="_blank">Jacob Collier</a
+			>, negative harmony is a theory that suggests that translating a
+			chord across a tonic/dominant axis will produce another chord with
+			the same potential function due to its intervallic structure in
+			relation to the tonic.
+		</p>
+		<figure class="figure d-flex flex-column align-items-center mb-3">
+			<img
+				class="img-fluid rounded-2"
+				src="https://forrestbalman.github.io/negative-harmony-calculator/media/images/apps/negative-harmony/axis-c.png"
+				alt="The tonic dominant axis C" />
+			<figcaption class="figure-caption text-light">
+				The tonic dominant axis for C
+			</figcaption>
+		</figure>
+		<p class="fs-4">
+			Although it's easier to visualize as a midway point between the
+			tonic and dominant, the axis that we're translating across is at
+			E-quarter-flat. If we take each chord tone and replace it with it
+			counterpart across this axis, we'll generate our negative harmony.
+			For example, A minor's negative counterpart is E-flat major. How
+			would you figure this out without using the calculator 🤔? By
+			translating each of the chord's tones across the axis:
+		</p>
+		<ol class="mb-3">
+			<li class="fs-4 fw-light">A would be converted to B-flat</li>
+			<li class="fs-4 fw-light">C would be converted to G</li>
+			<li class="fs-4 fw-light">E would be converted to E-flat</li>
+		</ol>
+		<p class="fs-4 mb-3">
+			If we follow the same procedure for all diatonic chords, we can make
+			a chart that should be true for all keys 😲:
+		</p>
+		<ul class="mb-3">
+			<li class="fs-4 fw-light">
+				<span class="serif">I</span> turns into
+				<span class="serif">i</span>
+			</li>
+			<li class="fs-4 fw-light">
+				<span class="serif">ii</span> turns into ♭<span class="serif"
+					>VII</span>
+			</li>
+			<li class="fs-4 fw-light">
+				<span class="serif">iii</span> turns into ♭<span class="serif"
+					>VI</span>
+			</li>
+			<li class="fs-4 fw-light">
+				<span class="serif">IV</span> turns into
+				<span class="serif">v</span>
+			</li>
+			<li class="fs-4 fw-light">
+				<span class="serif">V</span> turns into
+				<span class="serif">iv</span>
+			</li>
+			<li class="fs-4 fw-light">
+				<span class="serif">vi</span> turns into ♭<span class="serif"
+					>III</span>
+			</li>
+			<li class="fs-4 fw-light">
+				<span class="serif">vii°</span> turns into
+				<span class="serif">ii°</span>
+			</li>
+		</ul>
+		<p class="fs-4 mb-3">
+			Coincidentally, the chart above also covers the minor scale. Notice
+			how the translations from right to left procude the chords of the
+			natural minor scale. In the case of melodic minor, the chart
+			accounts for <span class="serif">ii</span> and
+			<span class="serif">vii°</span>, but not
+			<span class="serif">vi°</span>
+			and <span class="serif">III</span>+, because they require a
+			chromatic alteration that isn't included in the parallel major.
+		</p>
+		<ul class="mb-3">
+			<li class="fs-4">
+				<span class="serif">vi°</span> turns into
+				<span class="serif">iii°</span>
+			</li>
+			<li class="fs-4">
+				♭<span class="serif">III</span>+ turns into ♭<span class="serif"
+					>VI</span
+				>+
+			</li>
+		</ul>
+		<p class="fs-4 mb-3">
+			You can also use these shortcuts if you want to calculate negative
+			harmonies on the fly:
+		</p>
+		<ol class="mb-3">
+			<li class="fs-4 fw-light">
+				To get the scale degree of the negative harmony, subtract your
+				starting scale degree from 9. I.e 2 turns into 7 because 9 minus
+				2 is equal to 7.
+			</li>
+			<li class="fs-4 fw-light">
+				Major chords become minor, and vice versa.
+			</li>
+			<li class="fs-4 fw-light">
+				Diminished and augmented chords keep their quality.
+			</li>
+		</ol>
+		<p class="fs-4 mb-3">
+			With all that being said, you should be able to trust the chart, or
+			the quick and dirty method, above for every key; however let's test
+			it in the key of F major and see if the translations are correct.
+		</p>
+		<figure class="figure d-flex flex-column align-items-center mb-3">
+			<img
+				class="img-fluid rounded-2"
+				src="https://forrestbalman.github.io/negative-harmony-calculator/media/images/apps/negative-harmony/axis-f.png"
+				alt="The tonic dominant axis for F" />
+			<figcaption class="figure-caption text-light">
+				The tonic dominant axis for F
+			</figcaption>
+		</figure>
+		<p class="fs-4 mb-3">
+			Because we found the negative harmony for A minor in relation to C
+			major in the first example, let's try D minor here. As we
+			hypothesized above, <span class="serif">vi</span>'s negative
+			counterpart should be ♭<span class="serif">III</span>.
+		</p>
+		<ol class="mb-3">
+			<li class="fs-4 fw-light">D would be converted to E-flat</li>
+			<li class="fs-4 fw-light">F would be converted to C</li>
+			<li class="fs-4 fw-light">A would be converted to A-flat</li>
+		</ol>
+		<p class="fs-4 mb-3">
+			<a href="https://en.wikipedia.org/wiki/Ernst_Levy" target="_blank"
+				>Ernst Levy</a>
+			👨‍🏫 is the theorist credited with discovering this and detailing his
+			approach in his textbook
+			<a
+				class="fst-italic"
+				href="https://en.wikipedia.org/wiki/Ernst_Levy"
+				target="_blank">Theory of Harmony</a
+			>. However, after reading the book myself, although Levy goes into
+			detail about concepts such as
+			<span class="fst-italic">polarity</span>
+			and <span class="fst-italic">telluric gravity</span>, I couldn't
+			find a reference to this specific approach, which involves taking
+			the circle of fifths and rotating it to create an imaginary axis
+			between E-flat and E-natural.
+		</p>
+		<p class="fs-4 mb-3">
+			Instead, on page 25 for instance, we see that C remains the axis,
+			and that G translates to F, E to A-flat, etc. . Levy does comment on
+			the idea of the label <span class="fst-italic">subdominant</span> being
+			a fallacy due to both dominant and subdominant behaving similarly. But
+			other than this, I can't seem to find a passage that suggests this exact
+			method, despite it being the consensus method across the web 🌐.
+		</p>
+		<p class="fs-4">
+			In fact, I never saw the term <span class="fst-italic"
+				>negative harmony</span> used once; however, I think it is a fantastic
+			way to illustrate the harmonic relationships being theorized here. 🎵
+		</p>
+	</div>
 </section>
 
 <style>
-    a {
-        color: #cbaeeb;
-    }
+	a {
+		color: #cbaeeb;
+	}
 </style>
